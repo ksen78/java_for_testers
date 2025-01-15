350a95d2e9029dd7fa968d8b95fc033b56e27b85
Ksu17 <mal-ks17@mail.ru>
2025-01-15 14:11:44 +0300

    update Generator.java

--- addressbook_web_tests/src/test/java/generator/Generator.java
+++ addressbook_web_tests/src/test/java/generator/Generator.java
@@ -76,22 +76,32 @@ public class Generator {
     }
 
     private void save(Object data) throws IOException {
-        if ("json".equals(format)) {
+        if ( "json".equals(format)){
             ObjectMapper mapper = new ObjectMapper();
             mapper.enable(SerializationFeature.INDENT_OUTPUT);
             var json = mapper.writeValueAsString(data);
 
-            try (var writer = new FileWriter(output)) {
+            try (var writer = new FileWriter(output)){
                 writer.write(json);
+            } catch (IOException e) {
+                throw new IOException("Ошибка при записи JSON файла", e);
             }
-        } if ("yaml".equals(format)) {
+        } else if ("yaml".equals(format)) {
             var mapper = new YAMLMapper();
-            mapper.writeValue(new File(output), data);
-        } if ("xml".equals(format)) {
+            try {
+                mapper.writeValue(new File(output), data);
+            } catch (IOException e) {
+                throw new IOException("Ошибка при записи YAML файла", e);
+            }
+        } else if ("xml".equals(format)) {
             var mapper = new XmlMapper();
-            mapper.writeValue(new File(output), data);
+            try {
+                mapper.writeValue(new File(output), data);
+            } catch (IOException e) {
+                throw new IOException("Ошибка при записи XML файла", e);
+            }
         } else {
-            throw new IllegalArgumentException("Неизвестный формат данных " + format);
+            throw new IllegalArgumentException("Неизвестный формат данных" + format);
         }
     }
 }
